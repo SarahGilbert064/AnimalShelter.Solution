0060752863faee13fa933a75770ebe55321cdc9a
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-01-22 14:58:45 -0800

    all all get/post/put/delete methods into ApiHelper.cs

---
+++ AnimalShelterClient/Controllers/DogsController.cs
@@ -0,0 +1,16 @@
+// using System.Collections.Generic;
+// using System.Linq;
+// using Microsoft.AspNetCore.Mvc;
+// using AnimalShelterClient.Models;
+
+// namespace AnimalShelterClient.Controllers
+// {
+//   public class DogsController : Controller
+//   {
+//     public IActionResult Index()
+//     {
+//       var allDogs = Dog.GetDogs();
+//       return View(allDogs);
+//     }
+//   }
+// }

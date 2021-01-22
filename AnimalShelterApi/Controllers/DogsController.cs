f14fb5237f8430b740e46662ef8a52d6c3cd993b
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-01-22 10:51:21 -0800

    build out DogsController with all needed get/post/put/delete functions

---
+++ AnimalShelterApi/Controllers/DogsController.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelterApi.Models;
+
+namespace AnimalShelterApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DogsController : ControllerBase
+  {
+    private AnimalShelterApiContext _db;
+
+    public DogsController(AnimalShelterApiContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Dog>> Get(string name, string breed, string age)
+    {
+      var query = _db.Dogs.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (breed != null)
+      {
+        query = query.Where(entry => entry.Breed == breed);
+      }
+
+      if (age != null)
+      {
+        query = query.Where(entry => entry.Age == age);
+      }
+
+      return query.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Dog> Get(int id)
+    {
+      return _db.Dogs.FirstOrDefault(entry => entry.DogId == id);
+    }
+
+    [HttpPost]
+    public void Post([FromBody] Dog dog)
+    {
+      _db.Dogs.Add(dog);
+      _db.SaveChanges();
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Dog dog)
+    {
+      dog.DogId = id;
+      _db.Entry(dog).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var dogToDelete = _db.Dogs.FirstOrDefault(entry => entry.DogId == id);
+      _db.Dogs.Remove(dogToDelete);
+      _db.SaveChanges();
+    }
+    
+  }
+}

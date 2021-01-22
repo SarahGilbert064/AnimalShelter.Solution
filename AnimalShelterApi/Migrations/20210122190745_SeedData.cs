8d138dd9296c5359ee73f95f3d383bcf0282625d
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-01-22 11:08:54 -0800

    seed data for both cats and dogs in apiContext file and run migration database update

---
+++ AnimalShelterApi/Migrations/20210122190745_SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "Age", "Breed", "Name" },
+                values: new object[,]
+                {
+                    { 1, "2 years old", "Russian Blue", "Charlie" },
+                    { 2, "10 years old", "Tabby", "Max" },
+                    { 3, "6 months old", "Siamese", "Barry" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Breed", "Name" },
+                values: new object[,]
+                {
+                    { 1, "12 years old", "German Shepard", "Kika" },
+                    { 2, "6 years old", "Pitbull Mix", "Daisy" },
+                    { 3, "3 months old", "Siberian Husky", "Maya" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 3);
+        }
+    }
+}
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -9,6 +9,27 @@ namespace AnimalShelterApi.Models
       {
       }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Cat>()
+        .HasData(
+          new Cat { CatId = 1, Name = "Charlie", Breed = "Russian Blue", Age = "2 years old"},
+          new Cat { CatId = 2, Name = "Max", Breed = "Tabby", Age = "10 years old"},
+          new Cat { CatId = 3, Name = "Barry", Breed = "Siamese", Age = "6 months old"}
+        );
+
+      builder.Entity<Dog>()
+        .HasData(
+          new Dog { DogId = 1, Name = "Kika", Breed = "German Shepard", Age = "12 years old"},
+          new Dog { DogId = 2, Name = "Daisy", Breed = "Pitbull Mix", Age = "6 years old"},
+          new Dog { DogId = 3, Name = "Maya", Breed = "Siberian Husky", Age = "3 months old"}
+        );
+    }
       public DbSet<Dog> Dogs { get; set; }
       public DbSet<Cat> Cats { get; set; }
   }
